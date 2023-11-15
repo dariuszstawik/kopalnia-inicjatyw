3e5d72ccfb239ae49d74140b00703ea1a034c5fa
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-15 21:26:28 +0100

    Fix errors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["localhost:3000", "images.ctfassets.net"],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/pl",
+      },
+    ];
+  },
 };
 
 const withNextIntl = require("next-intl/plugin")("./i18n.js");
