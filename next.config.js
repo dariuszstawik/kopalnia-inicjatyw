8d57064ee07a00b61ae08358671323f6b61fdc3a
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-16 14:58:17 +0100

    Fix errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require("next-intl/plugin")("/i18n.js");
+const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
 // const nextConfig = {
 //   images: {
