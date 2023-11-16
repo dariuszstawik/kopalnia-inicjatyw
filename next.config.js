7f4a28998d58aefe9726f1203623d388af8de969
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-16 12:59:37 +0100

    Fix errors

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withNextIntl = require("next-intl/plugin")();
+
 const nextConfig = {
   images: {
     domains: ["localhost:3000", "images.ctfassets.net"],
   },
-  i18n: {
-    locales: ["en", "pl"],
-    defaultLocale: "pl",
-  },
 };
 
-const withNextIntl = require("next-intl/plugin")("./i18n.js");
+// const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
 module.exports = withNextIntl(nextConfig);
