b5c91f74070b03c380c91ac8441b99776c1862e4
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-16 13:55:32 +0100

    Fix erros

--- middleware.js
+++ middleware.js
@@ -1,16 +1,20 @@
 import createMiddleware from "next-intl/middleware";
+import { pathnames, locales } from "./config";
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ["en", "pl"],
+  // locales: ["en", "pl"],
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: "pl",
+  // defaultLocale: "pl",
+  defaultLocale: "en",
+  locales,
+  pathnames,
 });
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips
   // certain folders and all pathnames with a dot (e.g. favicon.ico)
-  // matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
-  matcher: ["/", "/(de|en)/:path*"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  // matcher: ["/", "/(de|en)/:path*"],
 };
