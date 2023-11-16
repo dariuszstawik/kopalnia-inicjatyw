f5fd12c74b3e93bada7dbf3e5b7a4f062d9d173a
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-16 14:01:52 +0100

    Fix errors

---
+++ config.js
@@ -0,0 +1,13 @@
+import { Pathnames } from "next-intl/navigation";
+
+export const locales = ["en", "de"];
+
+export const pathnames = {
+  "/": "/",
+  "/pathnames": {
+    en: "/pathnames",
+    pl: "/pfadnamen",
+  },
+};
+
+// export type AppPathnames = keyof typeof pathnames;
